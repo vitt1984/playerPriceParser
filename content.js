c89e46f8c0519472921602b1ee358939bcb8c633
Vittorio Turri <vturri@phmgmt.com>
2016-08-15 09:33:51 +0200

    content.js: assigned numeric values to all literal properties values and translated them to english; also added function to parse the deadline (timestamp)

--- content.js
+++ content.js
@@ -2,45 +2,77 @@ console.error('test content.js');
 
 // CONSTANTS
 
+let propertiesValues = {
+  // skills
+  'divine'            : 20,
+  'utopian'           : 19,
+  'magical'           : 18,
+  'mythical'          : 17,
+  'extra-terrestrial' : 16,
+  'titanic'           : 15,
+  'supernatural'      : 14,
+  'world class'       : 13,
+  'magnificent'       : 12,
+  'brilliant'         : 11,
+  'outstanding'       : 10,
+  'formidable'        : 9,
+  'excellent'         : 8,
+  'solid'             : 7,
+  'passable'          : 6,
+  'inadequate'        : 5,
+  'weak'              : 4,
+  'poor'              : 3,
+  'wretched'          : 2,
+  'disastrous'        : 1,
+  'non-existent'      : 0,
+  // specialties
+  'Technical'     : 5,
+  'Quick'         : 4,
+  'Head'          : 3,
+  'Powerful'      : 2,
+  'Unpredictable' : 1,
+  'Regainer'      : 0
+};
+
 let playerProperties = {
   'experience' : {
-    regex: /([a-z\s]+) in esperienza/g,
+    regex: /Has ([a-z\s]+) experience/g,
     numeric: false
   },
   'stamina'    : {
-    regex: /Resistenza:\s*([a-z\s]+\s)/g,
+    regex: /Stamina:\s*([a-z\s]+\s)/g,
     numeric: false
   },
   'playmaking' : {
-    regex: /Regia:\s*([a-z\s]+\s)/g,
+    regex: /Playmaking:\s*([a-z\s]+\s)/g,
     numeric: false
   },
   'winger'     : {
-    regex: /Cross:\s*([a-z\s]+\s)/g,
+    regex: /Winger:\s*([a-z\s]+\s)/g,
     numeric: false
   },
   'scoring'    : {
-    regex: /Attacco:\s*([a-z\s]+\s)/g,
+    regex: /Scoring:\s*([a-z\s]+\s)/g,
     numeric: false
   },
   'keeper'     : {
-    regex: /Parate:\s*([a-z\s]+\s)/g,
+    regex: /Keeper:\s*([a-z\s]+\s)/g,
     numeric: false
   },
   'passing'    : {
-    regex: /Passaggi:\s*([a-z\s]+\s)/g,
+    regex: /Passing:\s*([a-z\s]+\s)/g,
     numeric: false
   },
   'defending'  : {
-    regex: /Difesa:\s*([a-z\s]+\s)/g,
+    regex: /Defending:\s*([a-z\s]+\s)/g,
     numeric: false
   },
   'set pieces' : {
-    regex: /Calci Piazzati:\s*([a-z\s]+\s)/g,
+    regex: /Set pieces:\s*([a-z\s]+\s)/g,
     numeric: false
   },
   'specialty'  : {
-    regex: /Specialità:\s*([a-zA-Z\s]+\s)/g,
+    regex: /Specialty:\s*([a-zA-Z\s]+\s)/g,
     numeric: false
   },
   'price'      : {
@@ -53,7 +85,7 @@ let playerProperties = {
   }
 };
 let playerIdRegex = /playerId=([0-9]+)/g;
-let expiryRegex = /Scadenza:\s+([0-9]{2}-[0-9]{2}-[0-9]{4} [0-9]{2}\.[0-9]{2})/g;
+let deadlineRegex = /Deadline:\s+([0-9]{2})-([0-9]{2})-([0-9]{4}) ([0-9]{2})\.([0-9]{2})/g;
 
 var transferResultPage = /hattrick\.org\/World\/Transfers\/TransfersSearchResult\.aspx/g;
 
@@ -64,7 +96,7 @@ function getPlayerId( transferPlayerInfo ) {
   let playerIdMatch = playerIdRegex.exec( transferPlayerInfo.outerHTML );
   var playerid = undefined;
   if ( playerIdMatch ) {
-    playerid = playerIdMatch[1];
+    playerid = Number( playerIdMatch[1] );
   }
   // http://stackoverflow.com/questions/3891641/regex-test-only-works-every-other-time
   // due to a bug, we call the regex a second time
@@ -73,18 +105,19 @@ function getPlayerId( transferPlayerInfo ) {
   return playerid;
 }
 
-function getExpiryDate( transferPlayerInfo ) {
+function getDeadline( transferPlayerInfo ) {
 
-  let expiryMatch = expiryRegex.exec( transferPlayerInfo.outerText );
-  var expiry = undefined;
-  if ( expiryMatch ) {
-    expiry = expiryMatch[1];
+  let deadlineMatch = deadlineRegex.exec( transferPlayerInfo.outerText );
+  var deadline = undefined;
+  if ( deadlineMatch ) {
+    deadline = new Date (deadlineMatch[3].concat('-', deadlineMatch[2], '-', deadlineMatch[1], 'T',
+                         deadlineMatch[4], ':', deadlineMatch[5], ':00'));
   }
   // http://stackoverflow.com/questions/3891641/regex-test-only-works-every-other-time
   // due to a bug, we call the regex a second time
-  expiryRegex.exec( transferPlayerInfo.outerText );
+  deadlineRegex.exec( transferPlayerInfo.outerText );
 
-  return expiry;
+  return deadline;
 }
 
 function getPlayerInfo( transferPlayerInfo ) {
@@ -95,9 +128,13 @@ function getPlayerInfo( transferPlayerInfo ) {
     if ( matchingRegex ) {
       if ( playerProperties[property].numeric ) {
         value = Number(matchingRegex[1].replace(/\s/g, ''));
-      }
-      else {
-        value = matchingRegex[1].replace(/\s/g, '')
+      } else {
+        value = matchingRegex[1].replace(/^\s/g, '').replace(/\s$/g, '');
+        if ( value in propertiesValues ) {
+          value = propertiesValues[value];
+        } else {
+          console.warn('value', value,'could not be translated into numeric, saving raw');
+        }
       }
       playerProps[property] = value;
       //console.error(property, matchingRegex[1]);
@@ -143,7 +180,7 @@ function addPlayerToDb( player, store ) {
 
 // MAIN
 
-var openRequest = indexedDB.open('hattrickDb',4);
+var openRequest = indexedDB.open('hattrickDb',5);
 
 openRequest.onerror = function(e) {
   console.error('Error', e);
@@ -174,11 +211,11 @@ openRequest.onsuccess = function(e) {
 
       let player = {
         'playerId': getPlayerId( transferPlayerInfo ),
-        'expiry': getExpiryDate( transferPlayerInfo ),
+        'deadline': getDeadline( transferPlayerInfo ),
         'finalPrice': undefined
       };
 
-      if ( player.expiry ) {
+      if ( player.deadline ) {
         Object.assign(player, getPlayerInfo( transferPlayerInfo ));
 
         addPlayerToDb( player, store );
