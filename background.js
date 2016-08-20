c2bf034b6199520b8fb14ce32f3991ae3a49008a
Vittorio Turri <vturri@phmgmt.com>
2016-08-20 20:21:44 +0200

    background.js: corrected state creation error handling, and created searches for defenders

--- background.js
+++ background.js
@@ -17,67 +17,63 @@ chrome.browserAction.onClicked.addListener( (aTab) => {
 
   var searchesDb = new PouchDB('http://localhost:5984/hattrick_searches');
 
-  var promises = [
-    // searchesDb.put({
-    //   _id: '2',
-    //   defending: {
-    //     min: 14,
-    //     max: 17
-    //   },
-    //   passing: {
-    //     min: 6,
-    //     max: 9
-    //   },
-    //   age: {
-    //     min: 23,
-    //     max: 27
-    //   }
-    // }),
+  searchesDb.destroy().then() {
 
-  // searchesDb.put({
-  //   _id: '3',
-  //   playmaking: {
-  //     min: 14,
-  //     max: 17
-  //   },
-  //   passing: {
-  //     min: 6,
-  //     max: 9
-  //   },
-  //   age: {
-  //     min: 23,
-  //     max: 27
-  //   }
-  // });
+    var searches = [ 
+      {defending: {min: 11, max: 11}, passing: {max: 5}, age: {min: 20, max: 25}},
+      {defending: {min: 11, max: 11}, passing: {min: 6, max: 6}, age: {min: 20, max: 25}},
+      {defending: {min: 11, max: 11}, passing: {min: 7, max: 7}, age: {min: 20, max: 25}},
+      {defending: {min: 11, max: 11}, passing: {min: 8, max: 8}, age: {min: 20, max: 25}},
+      {defending: {min: 11, max: 11}, passing: {min: 9, max: 15}, age: {min: 21, max: 26}},
+      {defending: {min: 12, max: 12}, passing: {max: 5}, age: {min: 20, max: 25}},
+      {defending: {min: 12, max: 12}, passing: {min: 6, max: 6}, age: {min: 20, max: 25}},
+      {defending: {min: 12, max: 12}, passing: {min: 7, max: 7}, age: {min: 20, max: 25}},
+      {defending: {min: 12, max: 12}, passing: {min: 8, max: 8}, age: {min: 20, max: 25}},
+      {defending: {min: 12, max: 12}, passing: {min: 9, max: 15}, age: {min: 21, max: 26}},
+      {defending: {min: 13, max: 13}, passing: {max: 5}, age: {min: 20, max: 25}},
+      {defending: {min: 13, max: 13}, passing: {min: 6, max: 6}, age: {min: 20, max: 25}},
+      {defending: {min: 13, max: 13}, passing: {min: 7, max: 7}, age: {min: 20, max: 25}},
+      {defending: {min: 13, max: 13}, passing: {min: 8, max: 8}, age: {min: 20, max: 25}},
+      {defending: {min: 13, max: 13}, passing: {min: 9, max: 15}, age: {min: 21, max: 26}},
+      {defending: {min: 14, max: 14}, passing: {max: 5}, age: {min: 20, max: 25}},
+      {defending: {min: 14, max: 14}, passing: {min: 6, max: 6}, age: {min: 20, max: 25}},
+      {defending: {min: 14, max: 14}, passing: {min: 7, max: 7}, age: {min: 20, max: 25}},
+      {defending: {min: 14, max: 14}, passing: {min: 8, max: 8}, age: {min: 20, max: 25}},
+      {defending: {min: 14, max: 14}, passing: {min: 9, max: 15}, age: {min: 21, max: 26}},
+    ]
 
-  ];
+    var promises = [];
 
-  var stateDb = new PouchDB('http://localhost:5984/hattrick_state');
+    var id = 0;
+    for (var search in searches) {
+      search._id = id;
+      id = id + 1;
+      promises.push( searchesDb.put(search) );
+    }
 
-  Promise.all( promises ).then( () => {
-    stateDb.get('state').then( ( currentState ) => {
-      console.error('background currentState', currentState);
-      // if ( !currentState ) {
-      //   stateDb.put({_id: 'state', state: 'PLAYERS_CHECK'}).then( () => {
-      //     chrome.tabs.sendMessage(aTab.id, {}, doStuffWithDOM);
-      //   });
-      // } else 
-      if ( currentState && currentState.state === 'INACTIVE' ) {
-        currentState.state = 'PLAYERS_CHECK';
-        stateDb.put( currentState ).then( () => {
+    var stateDb = new PouchDB('http://localhost:5984/hattrick_state');
+
+    Promise.all( promises ).then( () => {
+      stateDb.get('state').then( ( currentState ) => {
+        console.error('background currentState', currentState);
+        if ( currentState && currentState.state === 'INACTIVE' ) {
+          currentState.state = 'PLAYERS_CHECK';
+          stateDb.put( currentState ).then( () => {
+            chrome.tabs.sendMessage(aTab.id, {}, doStuffWithDOM);
+          });
+        } else {
+          console.error('state is not inactive');
+        }
+      }).catch( ( error ) => {
+      console.error('evil cach');
+        stateDb.put({_id: 'state', state: 'PLAYERS_CHECK'}).then( () => {
+          console.error('create state for 1st time');
           chrome.tabs.sendMessage(aTab.id, {}, doStuffWithDOM);
         });
-      } else {
-        console.error('state is not inactive');
-      }
-    }).catch( ( error ) => {
-    console.error('evil cach');
-      stateDb.put({_id: 'state', state: 'PLAYERS_CHECK'}).then( () => {
-        console.error('create state for 1st time');
-        chrome.tabs.sendMessage(aTab.id, {}, doStuffWithDOM);
       });
     });
-  });
+
+  }
 
   //chrome.tabs.create({'url': 'http://chilloutandwatchsomecatgifs.com/', 'active': true});
 
