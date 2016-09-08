ca039d74e53e329ac941d68733565e7c6f88485b
Vittorio Turri <vturri@phmgmt.com>
2016-09-08 20:19:52 +0200

    constants.js: add automatic server name resolution

--- constants.js
+++ constants.js
@@ -1,2 +1,16 @@
-SERVER_POSTFIX = '92';
+
+let postfixRegex = /www([0-9]+)\.hattrick\.org/g;
+
+function getServerPostfix() {
+
+  let postfixMatch = postfixRegex.exec( window.location.href );
+  var postfix = undefined;
+  if ( postfixMatch ) {
+    postfix = postfixMatch[1];
+  }
+
+  return postfix;
+}
+
+SERVER_POSTFIX = getServerPostfix();
 SERVER = 'https://www'.concat(SERVER_POSTFIX, '.hattrick.org');
