86f672fca86c7a2ea2ddfe03043bd1962fabf62a
Vittorio Turri <vturri@phmgmt.com>
2017-04-02 18:45:27 +0200

    background.js: added search criteria for `playmaking`

--- background.js
+++ background.js
@@ -1,18 +1,5 @@
 'use strict';
 
-/*global chrome:false */
-
-// chrome.browserAction.setBadgeText({text: '(ツ)'});
-// chrome.browserAction.setBadgeBackgroundColor({color: '#eae'});
-
-//http://stackoverflow.com/questions/28858027/how-to-read-file-from-chrome-extension
-//http://stackoverflow.com/questions/21314897/access-dom-elements-through-chrome-extension
-
-/* A function creator for callbacks */
-function doStuffWithDOM(element) {
-  //alert("I received the following DOM content:\n" + element);
-}
-
 function createPlayerWithoutFinalPriceView() {
 
   var playersDb = new PouchDB('http://localhost:5984/hattrick');
@@ -78,7 +65,6 @@ function createSkillsView() {
 
 };
 
-
 // http://stackoverflow.com/questions/5731193/how-to-format-numbers-using-javascript
 // TODO shared function
 function formatNumber(number) {
@@ -93,6 +79,9 @@ function formatNumber(number) {
   return x1 + x2;
 };
 
+function emptyResponse(element) {
+  // nothing to do
+}
 
 var review = false;
 
@@ -101,14 +90,6 @@ if ( review ) {
   chrome.browserAction.onClicked.addListener( (aTab) => {
 
     var playersDb = new PouchDB('http://localhost:5984/hattrick');
-
-    // playersDb.query('skillsView/defending', {
-    //   key: [21, 11, 0, 0, 0], group: true
-    // }).then(function (result) {
-    //   console.error('result', result)
-    // }).catch(function (err) {
-    //   console.error('err', err)
-    // });
 
     playersDb.query(function (doc) {
       if ( doc.price < doc.priceForProfit2days &&
@@ -171,12 +152,6 @@ if ( review ) {
       console.error('err', err);
     });
 
-    // playersDb.compact().then( ( result ) => {
-    //   console.error(result);
-    // }).catch(function (err) {
-    //   console.log(err);
-    // });
-
   });
 
 } else {
@@ -189,6 +164,7 @@ if ( review ) {
       searchesDb = new PouchDB('http://localhost:5984/hattrick_searches');
 
       var searches = [
+        // defending
         {defending: {min: 11, max: 11}, passing: {max: 5}, age: {min: 20, max: 20}},
         {defending: {min: 11, max: 11}, passing: {max: 5}, age: {min: 21, max: 21}},
         {defending: {min: 11, max: 11}, passing: {max: 5}, age: {min: 22, max: 22}},
@@ -229,6 +205,47 @@ if ( review ) {
         {defending: {min: 14, max: 14}, passing: {min: 7, max: 7}, age: {min: 23, max: 25}},
         {defending: {min: 14, max: 14}, passing: {min: 8, max: 8}, age: {min: 20, max: 25}},
         {defending: {min: 14, max: 14}, passing: {min: 9, max: 15}, age: {min: 21, max: 26}},
+        // playmaking
+        {playmaking: {min: 11, max: 11}, passing: {max: 5}, age: {min: 20, max: 20}},
+        {playmaking: {min: 11, max: 11}, passing: {max: 5}, age: {min: 21, max: 21}},
+        {playmaking: {min: 11, max: 11}, passing: {max: 5}, age: {min: 22, max: 22}},
+        {playmaking: {min: 11, max: 11}, passing: {max: 5}, age: {min: 23, max: 25}},
+        {playmaking: {min: 11, max: 11}, passing: {min: 6, max: 6}, age: {min: 20, max: 22}},
+        {playmaking: {min: 11, max: 11}, passing: {min: 7, max: 7}, age: {min: 20, max: 22}},
+        {playmaking: {min: 11, max: 11}, passing: {min: 6, max: 6}, age: {min: 23, max: 25}},
+        {playmaking: {min: 11, max: 11}, passing: {min: 7, max: 7}, age: {min: 23, max: 25}},
+        {playmaking: {min: 11, max: 11}, passing: {min: 8, max: 8}, age: {min: 20, max: 25}},
+        {playmaking: {min: 11, max: 11}, passing: {min: 9, max: 15}, age: {min: 21, max: 26}},
+        {playmaking: {min: 12, max: 12}, passing: {max: 5}, age: {min: 20, max: 20}},
+        {playmaking: {min: 12, max: 12}, passing: {max: 5}, age: {min: 21, max: 21}},
+        {playmaking: {min: 12, max: 12}, passing: {max: 5}, age: {min: 22, max: 22}},
+        {playmaking: {min: 12, max: 12}, passing: {max: 5}, age: {min: 23, max: 25}},
+        {playmaking: {min: 12, max: 12}, passing: {min: 6, max: 6}, age: {min: 20, max: 22}},
+        {playmaking: {min: 12, max: 12}, passing: {min: 7, max: 7}, age: {min: 20, max: 22}},
+        {playmaking: {min: 12, max: 12}, passing: {min: 6, max: 6}, age: {min: 23, max: 25}},
+        {playmaking: {min: 12, max: 12}, passing: {min: 7, max: 7}, age: {min: 23, max: 25}},
+        {playmaking: {min: 12, max: 12}, passing: {min: 8, max: 8}, age: {min: 20, max: 25}},
+        {playmaking: {min: 12, max: 12}, passing: {min: 9, max: 15}, age: {min: 21, max: 26}},
+        {playmaking: {min: 13, max: 13}, passing: {max: 5}, age: {min: 20, max: 20}},
+        {playmaking: {min: 13, max: 13}, passing: {max: 5}, age: {min: 21, max: 21}},
+        {playmaking: {min: 13, max: 13}, passing: {max: 5}, age: {min: 22, max: 22}},
+        {playmaking: {min: 13, max: 13}, passing: {max: 5}, age: {min: 23, max: 25}},
+        {playmaking: {min: 13, max: 13}, passing: {min: 6, max: 6}, age: {min: 20, max: 22}},
+        {playmaking: {min: 13, max: 13}, passing: {min: 7, max: 7}, age: {min: 20, max: 22}},
+        {playmaking: {min: 13, max: 13}, passing: {min: 6, max: 6}, age: {min: 23, max: 25}},
+        {playmaking: {min: 13, max: 13}, passing: {min: 7, max: 7}, age: {min: 23, max: 25}},
+        {playmaking: {min: 13, max: 13}, passing: {min: 8, max: 8}, age: {min: 20, max: 25}},
+        {playmaking: {min: 13, max: 13}, passing: {min: 9, max: 15}, age: {min: 21, max: 26}},
+        {playmaking: {min: 14, max: 14}, passing: {max: 5}, age: {min: 20, max: 20}},
+        {playmaking: {min: 14, max: 14}, passing: {max: 5}, age: {min: 21, max: 21}},
+        {playmaking: {min: 14, max: 14}, passing: {max: 5}, age: {min: 22, max: 22}},
+        {playmaking: {min: 14, max: 14}, passing: {max: 5}, age: {min: 23, max: 25}},
+        {playmaking: {min: 14, max: 14}, passing: {min: 6, max: 6}, age: {min: 20, max: 22}},
+        {playmaking: {min: 14, max: 14}, passing: {min: 7, max: 7}, age: {min: 20, max: 22}},
+        {playmaking: {min: 14, max: 14}, passing: {min: 6, max: 6}, age: {min: 23, max: 25}},
+        {playmaking: {min: 14, max: 14}, passing: {min: 7, max: 7}, age: {min: 23, max: 25}},
+        {playmaking: {min: 14, max: 14}, passing: {min: 8, max: 8}, age: {min: 20, max: 25}},
+        {playmaking: {min: 14, max: 14}, passing: {min: 9, max: 15}, age: {min: 21, max: 26}},
       ];
 
       var promises = [];
@@ -247,20 +264,19 @@ if ( review ) {
 
       Promise.all( promises ).then( () => {
         stateDb.get('state').then( ( currentState ) => {
-          console.error('background currentState', currentState);
+          console.error('Current state of the DB: ', currentState);
           if ( currentState && currentState.state === 'INACTIVE' ) {
             currentState.state = 'PLAYERS_CHECK';
             stateDb.put( currentState ).then( () => {
-              chrome.tabs.sendMessage(aTab.id, {}, doStuffWithDOM);
+              chrome.tabs.sendMessage(aTab.id, {}, emptyResponse);
             });
           } else {
             console.error('state is not inactive');
           }
         }).catch( ( error ) => {
-        console.error('evil cach');
           stateDb.put({_id: 'state', state: 'PLAYERS_CHECK'}).then( () => {
             console.error('create state for 1st time');
-            chrome.tabs.sendMessage(aTab.id, {}, doStuffWithDOM);
+            chrome.tabs.sendMessage(aTab.id, {}, emptyResponse);
           });
         });
       });
@@ -269,34 +285,4 @@ if ( review ) {
 
   });
 
-}
-
-
-
-
-  //chrome.tabs.create({'url': 'http://chilloutandwatchsomecatgifs.com/', 'active': true});
-
-  // chrome.extension.getBackgroundPage().console.log('foo');
-  // chrome.extension.getBackgroundPage().console.log(aTab);
-
-// chrome.browserAction.onClicked.addListener(function(aTab) {
-//   chrome.tabs.query({'url': 'http://chilloutandwatchsomecatgifs.com/'}, (tabs) => {
-//     if (tabs.length === 0) {
-//       // There is no catgif tab!
-//       chrome.tabs.create({'url': 'http://chilloutandwatchsomecatgifs.com/', 'active': true});
-//     } else {
-//       // Do something here…
-//       chrome.tabs.query({'url': 'http://chilloutandwatchsomecatgifs.com/', 'active': true}, (active) => {
-//         if (active.length === 0) {
-//           chrome.tabs.update(tabs[0].id, {'active': true});
-//         }
-//       });
-//     }
-//   });
-// });
-
-
-/* When the browser-action button is clicked... */
-// chrome.browserAction.onClicked.addListener(function(tab) {
-//   console.error('tab', tab);
-// });
+};
